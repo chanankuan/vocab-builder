cb0cc360a7ec4f0cbc1f3a7f1496ee64bec8dd92
Anton Chan <chanankuan1@gmail.com>
2024-07-03 11:10:55 +0200

    feat: update definitions

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,7 +1,24 @@
-export interface Register {
+export interface SignupRequest {
   name: string;
   email: string;
   password: string;
 }
 
-export interface Login extends Pick<Register, 'email' | 'password'> {}
+export interface SigninRequest
+  extends Pick<SignupRequest, 'email' | 'password'> {}
+
+export interface SignupResponse {
+  email: string;
+  name: string;
+  token: string;
+}
+
+export interface SigninResponse extends SignupResponse {}
+
+export interface SignoutResponse {
+  message: string;
+}
+
+export interface getMeResponse extends SignupResponse {
+  _id: string;
+}
