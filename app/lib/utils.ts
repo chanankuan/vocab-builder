d7cadf3c63560d6d91d4278d2ff47f660edf6d74
Anton Chan <chanankuan1@gmail.com>
2024-07-03 11:16:32 +0200

    fix: update register and login schemas

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -2,25 +2,25 @@ import * as Yup from 'yup';
 
 export const RegisterSchema = Yup.object().shape({
   name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+    .required('Name is a required field')
+    .min(2, 'Name must be at least 2 characters long')
+    .max(50, 'Name must be at most 50 characters long'),
   email: Yup.string()
-    .email('Invalid email')
-    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email')
-    .required('Required'),
+    .required('Email is a required field')
+    .email('Invalid email format')
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email format'),
   password: Yup.string()
+    .required('Password is a required field')
     .matches(
       /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/,
-      'Password must be 7 characters long, include at least 6 letters and 1 digit, and contain only letters and digits.'
-    )
-    .required('Required'),
+      'Password must be 7 characters long and include 6 letters and 1 digit.'
+    ),
 });
 
 export const LoginSchema = Yup.object().shape({
   email: Yup.string()
+    .required('Email is a required field')
     .email('Invalid email')
-    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email')
-    .required('Required'),
-  password: Yup.string().required(),
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email'),
+  password: Yup.string().required('Password is a required field'),
 });
