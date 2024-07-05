2e727657aedc7184dc95410ce6ef1770e1e567ae
Anton Chan <chanankuan1@gmail.com>
2024-07-05 13:15:43 +0200

    feat: add header and sidebar components

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,26 +1,100 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import logo from '@/public/images/logo.svg';
 import Link from 'next/link';
+import clsx from 'clsx';
+
+import Sidebar from './sidebar';
+import Navbar from './navbar';
+import LogoutButton from './logout-button';
+import UserBar from './user-bar';
+
+import logo from '@/public/images/logo.svg';
+import burger from '@/public/images/burger.svg';
+import type { User } from '@/app/lib/definitions';
 
 export default function Header() {
+  const [user, setUser] = useState<User | null>(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  function handleCloseSidebar() {
+    setIsSidebarOpen(false);
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1440) {
+        setIsSidebarOpen(false);
+      }
+    };
+
+    // Check on initial render
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setIsSidebarOpen]);
+
+  useEffect(() => {
+    const data = localStorage.getItem('user') || '';
+    if (data) {
+      const user = JSON.parse(data);
+      setUser(user);
+    }
+  }, []);
+
   return (
     <header>
-      <div className="container py-4 md:py-6">
-        <div className="flex items-center">
-          <Link href="/" className="flex items-center gap-x-4">
-            <Image
-              src={logo}
-              alt="Company logo"
-              height={36}
-              width={36}
-              priority
-            />
-            <span className="font-semibold text-lg leading-6">
-              VocabBuilder
-            </span>
-          </Link>
+      <div className="container py-4 md:py-5">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <Link href="/" className="flex items-center gap-x-4">
+              <Image
+                className="w-9 h-9 md:w-10 md:h-10"
+                src={logo}
+                alt="Company logo"
+                priority
+              />
+              <span className="font-semibold text-[18px] leading-[24px] md:text-[22px] md:leading-[32px]">
+                VocabBuilder
+              </span>
+            </Link>
+          </div>
+
+          <div className="max-lg:hidden">
+            <Navbar />
+          </div>
+
+          <div
+            className={clsx(
+              'flex items-center gap-2 md:gap-4',
+              isSidebarOpen && 'opacity-0'
+            )}
+          >
+            <UserBar isOpen={isSidebarOpen} user={user} />
+            <div className="max-lg:hidden lg:ml-2">
+              <LogoutButton />
+            </div>
+            <div className="flex items-center md:ml-3 lg:hidden">
+              <button onClick={() => setIsSidebarOpen(true)}>
+                <Image src={burger} alt="burger menu icon" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
+
+      <Sidebar
+        user={user}
+        isOpen={isSidebarOpen}
+        onCloseSidebar={handleCloseSidebar}
+      />
     </header>
   );
 }
---
+++ app/ui/header/user-bar.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import clsx from 'clsx';
+
+import avatar from '@/public/images/default_avatar.svg';
+import avatar_reversed from '@/public/images/default_avatar_reversed.svg';
+import type { User } from '@/app/lib/definitions';
+
+export default function UserBar({
+  user,
+  isOpen,
+}: {
+  user: User | null;
+  isOpen: boolean;
+}) {
+  return (
+    <>
+      <p
+        className={clsx(
+          'font-medium',
+          isOpen ? 'text-secondaryFont' : 'text-mainFont'
+        )}
+      >
+        {user?.name}
+      </p>
+      <div
+        className={clsx(
+          'w-9 h-9 md:w-12 md:h-12 rounded-full flex justify-center items-center',
+          isOpen ? 'bg-secondaryFont' : 'bg-green-dark'
+        )}
+      >
+        <Image
+          className="w-5 h-5 md:w-6 md:h-6"
+          src={isOpen ? avatar_reversed : avatar}
+          alt="default avatar"
+        />
+      </div>
+    </>
+  );
+}
