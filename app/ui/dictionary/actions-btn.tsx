e29dd93acdcb1d1292ee3eaa6e84c09a95593ce5
Anton Chan <chanankuan1@gmail.com>
2024-07-25 13:44:04 +0200

    fix:update actions component

--- app/ui/dictionary/actions-btn.tsx
+++ app/ui/dictionary/actions-btn.tsx
@@ -1,6 +1,13 @@
-export default function ActionsBtn({ action }: { action: () => void }) {
+export default function ActionsBtn({
+  action,
+}: {
+  action: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}) {
   return (
-    <button className="text-gray-dark hover:text-mainFont" onClick={action}>
+    <button
+      className="relative text-gray-dark transition-colors hover:text-mainFont"
+      onClick={action}
+    >
       ...
     </button>
   );
