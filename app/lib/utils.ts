4c2fed9a7a20e68e7342c14e62e40ac7d5f16d9e
Anton Chan <chanankuan1@gmail.com>
2024-07-25 13:20:47 +0200

    feat:add toast provider and showToast fn

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,7 @@
 import * as Yup from 'yup';
+import { toast, ToastContent, ToastOptions, Id, Bounce } from 'react-toastify';
+
+import { enRegex, uaRegex } from './constants';
 
 export const RegisterSchema = Yup.object().shape({
   name: Yup.string()
@@ -25,6 +28,70 @@ export const LoginSchema = Yup.object().shape({
   password: Yup.string().required('Password is a required field'),
 });
 
+export const CreateWordSchema = Yup.object().shape({
+  en: Yup.string()
+    .matches(enRegex)
+    .required('This field is required')
+    .test(
+      'is-irregular-format',
+      'Must be in the format form-II form-III form',
+      function (value) {
+        const { isIrregular } = this.parent;
+        if (isIrregular) {
+          // Check if the value matches the pattern word-word-word
+          return /^[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+$/.test(value);
+        }
+        // If isIrregular is not true, skip this test
+        return true;
+      }
+    ),
+  ua: Yup.string().matches(uaRegex).required('This field is required'),
+  category: Yup.string().required('This field is required'),
+  isIrregular: Yup.boolean()
+    .nullable()
+    .test('isIrregular', 'This field is required', function (value) {
+      const { category } = this.parent;
+      if (category === 'verb') {
+        // Ensure isIrregular is either true or false (i.e., a boolean)
+        return value === true || value === false;
+      }
+      // If category is not 'verb', consider the field valid regardless of its value
+      return true;
+    }),
+});
+
+export const EditWordSchema = Yup.object().shape({
+  en: Yup.string()
+    .matches(enRegex)
+    .required('This field is required')
+    .test(
+      'is-irregular-format',
+      'Must be in the format form-II form-III form',
+      function (value) {
+        const { isIrregular } = this.parent;
+        if (isIrregular) {
+          // Check if the value matches the pattern word-word-word
+          return /^[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+$/.test(value);
+        }
+        // If isIrregular is not true, skip this test
+        return true;
+      }
+    ),
+  ua: Yup.string().matches(uaRegex).required('This field is required'),
+  category: Yup.string().required('This field is required'),
+  isIrregular: Yup.boolean()
+    .nullable()
+    .test('isIrregular', 'This field is required', function (value) {
+      const { category } = this.parent;
+      if (category === 'verb') {
+        // Ensure isIrregular is either true or false (i.e., a boolean)
+        return value === true || value === false;
+      }
+      // If category is not 'verb', consider the field valid regardless of its value
+      return true;
+    }),
+});
+
 export function generatePagination(currentPage: number, totalPages: number) {
   // If the total number of pages is 4 or less,
   // display all pages without any ellipsis.
@@ -49,3 +116,54 @@ export function generatePagination(currentPage: number, totalPages: number) {
   // another ellipsis, and the last page.
   return [currentPage - 1, currentPage, currentPage + 1, '...', totalPages];
 }
+
+export function removeEmpty<T extends Record<string, any>>(obj: T): Partial<T> {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, v]) => v != '')
+  ) as Partial<T>;
+}
+
+export const defaultToastOptions: ToastOptions = {
+  position: 'top-right',
+  autoClose: 3000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+  transition: Bounce,
+};
+
+type ToastType = 'success' | 'error' | 'info' | 'warning' | 'default';
+
+/**
+ * Display toast
+ *
+ * @param {ToastType} type
+ * @param {ToastContent} content
+ * @param {ToastOptions} [options=defaultToastOption]
+ * @return {Id}
+ */
+export const showToast = (
+  type: ToastType,
+  content: ToastContent,
+  options: Partial<ToastOptions> = {}
+): Id => {
+  const optionsToApply = { ...defaultToastOptions, ...options };
+
+  switch (type) {
+    case 'success':
+      return toast.success(content, optionsToApply);
+    case 'error':
+      return toast.error(content, optionsToApply);
+    case 'info':
+      return toast.info(content, optionsToApply);
+    case 'warning':
+      return toast.warn(content, optionsToApply);
+    case 'default':
+      return toast(content, optionsToApply);
+    default:
+      return toast(content, optionsToApply);
+  }
+};
